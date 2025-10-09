0483b0b1ac33260f923fce9aaaace9cbc1fdb794
Riari <3583774+Riari@users.noreply.github.com>
2025-10-09 23:21:08 +0100

    Fix some usage issues in nvim

--- src/days/01/01.c
+++ src/days/01/01.c
@@ -7,31 +7,35 @@
 #include "utils.h"
 
 
-void day01_part1(void) {
-    char *input = read_input_file("input.txt");
+void day01_part1(void)
+{
+    char *input = read_input_file("01_input.txt");
     if (!input) return;
-    
 
+    free(input);
 }
 
-void day01_part2(void) {
-    char *input = read_input_file("input.txt");
+void day01_part2(void)
+{
+    char *input = read_input_file("01_input.txt");
     if (!input) return;
 
-
+    free(input);
 }
 
 bool day01_test_part1(void) {
-    char *input = read_input_file("test_input.txt");
+    char *input = read_input_file("01_test_input.txt");
     if (!input) return false;
 
+    free(input);
     return false;
 }
 
 bool day01_test_part2(void) {
-    char *input = read_input_file("test_input.txt");
+    char *input = read_input_file("01_test_input.txt");
     if (!input) return false;
 
+    free(input);
     return false;
 }
 
